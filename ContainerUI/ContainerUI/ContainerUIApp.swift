f7e0e3d614ca63fa30d3189633a5511ad60e0789
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2025-06-16 16:29:55 +0800

    init

---
+++ ContainerUI/ContainerUI/ContainerUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  ContainerUIApp.swift
+//  ContainerUI
+//
+//  Created by 甜檸Citron(lcandy2) on 6/16/25.
+//  Copyright © 2025 https://github.com/lcandy2. All Rights Reserved.
+//
+
+import SwiftUI
+
+@main
+struct ContainerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContainerUI/ContainerUI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ContainerUI
+//
+//  Created by 甜檸Citron(lcandy2) on 6/16/25.
+//  Copyright © 2025 https://github.com/lcandy2. All Rights Reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
